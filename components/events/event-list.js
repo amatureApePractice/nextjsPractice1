344ed83082aa7f5372d676516de556d326a0ba9c
drdarian <dariantaban@gmail.com>
2021-08-23 21:58:21 -0700

    built button

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -5,7 +5,7 @@ export default function EventList(props) {
   const { items } = props;
 
   return (
-    <ul>
+    <ul className={classes.list}>
       {items.map(event => <EventItem key={event.id} id={event.id} title={event.title} location={event.location} date={event.date} image={event.image}/>)}
     </ul>
   )
