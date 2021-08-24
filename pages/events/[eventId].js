df3dc7be968e675ec0a13b6512e9dc623f2b19ed
drdarian <dariantaban@gmail.com>
2021-08-24 14:59:39 -0700

    added event-detail components and styling

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,19 @@
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-data';
+
 export default function EventDetailPage() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return (
+      <p>No event found!</p>
+    )
+  }
+
   return (
     <div>
       <h1>Event Detail</h1>
